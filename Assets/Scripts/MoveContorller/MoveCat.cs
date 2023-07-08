d1e43d52df423a01a35efc128b5e1c965f75751a
Ice <577114589@qq.com>
2023-07-08 12:08:47 +0800

    提交路点

---
+++ Assets/Scripts/MoveContorller/MoveCat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCat : MonoBehaviour
+{
+    public List<MoveItem> moveInfo = new List<MoveItem>();
+    public int moveIndex = 0 ;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
